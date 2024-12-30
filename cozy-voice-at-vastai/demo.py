b703c534819f99ba6548522581181e212d95d688
jims57 <jims57@163.com>
2024-12-30 21:06:40 +0800

    1

---
+++ cozy-voice-at-vastai/demo.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('third_party/Matcha-TTS')
+from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
+from cosyvoice.utils.file_utils import load_wav
+import torchaudio
+
