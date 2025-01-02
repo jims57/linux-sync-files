1abfd10701f9829cbf759ed39b6c931f643173ff
jims57 <jims57@163.com>
2025-01-02 13:44:35 +0800

    Update demo.py

--- cosy-voice-at-aliyun-fc3-singapore/demo.py
+++ cosy-voice-at-aliyun-fc3-singapore/demo.py
@@ -7,7 +7,7 @@ import torchaudio
 cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=True, load_onnx=False, load_trt=False)
 
 prompt_speech_16k = load_wav('zero_shot_prompt.wav', 16000)
-for i, j in enumerate(cosyvoice.inference_zero_shot('景元，你好。十四年前我创办小米的时候,讲过一句话。我说当台风来的时候连猪都会飞,讲的是大家有像猪一样的态度,你就可以成功。把握机遇的重要性非常重要。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
+for i, j in enumerate(cosyvoice.inference_zero_shot('甘景元，你好。十四年前我创办小米的时候,讲过一句话。我说当台风来的时候连猪都会飞,讲的是大家有像猪一样的态度,你就可以成功。把握机遇的重要性非常重要。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
     # torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
     output_dir = 'pretrained_models'
     os.makedirs(output_dir, exist_ok=True)
