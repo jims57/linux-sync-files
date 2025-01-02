0df112cfffc5496bff3ef1cc4bd60ea78db6f855
jims57 <jims57@163.com>
2025-01-02 13:19:44 +0800

    22

---
+++ cosy-voice-at-aliyun-fc3-singapore/demo.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('third_party/Matcha-TTS')
+from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
+from cosyvoice.utils.file_utils import load_wav
+import torchaudio
+
+cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=True, load_onnx=False, load_trt=False)
+
+prompt_speech_16k = load_wav('zero_shot_prompt.wav', 16000)
+for i, j in enumerate(cosyvoice.inference_zero_shot('十四年前我创办小米的时候,讲过一句话。我说当台风来的时候连猪都会飞,讲的是大家有像猪一样的态度,你就可以成功。把握机遇的重要性非常重要。', '用粤语说这句话。', prompt_speech_16k, stream=False)):
+    torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
