2a41938f345ea775de2687a689ea16cf5dc772eb
jims57 <jims57@163.com>
2025-01-02 13:40:56 +0800

    2

--- cosy-voice-at-aliyun-fc3-singapore/demo.py
+++ cosy-voice-at-aliyun-fc3-singapore/demo.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 sys.path.append('third_party/Matcha-TTS')
 from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
 from cosyvoice.utils.file_utils import load_wav
@@ -8,4 +8,9 @@ cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=True, load_
 
 prompt_speech_16k = load_wav('zero_shot_prompt.wav', 16000)
 for i, j in enumerate(cosyvoice.inference_zero_shot('景元，你好。十四年前我创办小米的时候,讲过一句话。我说当台风来的时候连猪都会飞,讲的是大家有像猪一样的态度,你就可以成功。把握机遇的重要性非常重要。', '用粤语说这句话。', prompt_speech_16k, stream=False)):
-    torchaudio.save('\pretrained_models\audios\zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+    # torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+    output_dir = 'pretrained_models'
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Save the wav file to the pretrained_models directory
+    torchaudio.save(os.path.join(output_dir, 'zero_shot_{}.wav'.format(i)), j['tts_speech'], cosyvoice.sample_rate)
