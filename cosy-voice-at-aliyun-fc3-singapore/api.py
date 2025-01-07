92a9b6ef239619b993c58080e881957e182dad14
jims57 <jims57@163.com>
2025-01-07 14:59:43 +0800

    tts stream via websocket works.

--- cosy-voice-at-aliyun-fc3-singapore/api.py
+++ cosy-voice-at-aliyun-fc3-singapore/api.py
@@ -59,4 +59,4 @@ async def tts_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=50000)
